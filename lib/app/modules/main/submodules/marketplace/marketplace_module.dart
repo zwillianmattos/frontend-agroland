d799625f340e0d090e04e8610377e77bf9342560
Willian de Mattos Silva <willian_matt@hotmail.com>
2021-07-07 20:13:44 -0300

    fix: resolve bugs layout

--- lib/app/modules/main/submodules/marketplace/marketplace_module.dart
+++ lib/app/modules/main/submodules/marketplace/marketplace_module.dart
@@ -1,6 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:plant_care/app/core/env/variables.dart';
 import 'package:flutter_modular/flutter_modular.dart';
+import 'package:plant_care/app/modules/main/submodules/marketplace/pages/marketplace_home_store.dart';
 import 'package:plant_care/app/modules/main/submodules/marketplace/pages/marketplace_view_account_page.dart';
 import 'package:plant_care/app/modules/main/submodules/marketplace/pages/marketplace_view_page.dart';
 import './pages/marketplace_home_page.dart';
@@ -11,6 +12,7 @@ class MarketplaceModule extends Module {
     BindInject(
       (i) => Dio(BaseOptions(baseUrl: API_ENDPOINT)),
     ),
+    BindInject((i) => MarketplaceHomeStore())
   ];
 
   @override
